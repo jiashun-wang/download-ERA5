2ec46830cfce9229714a4b974a2ecb89b87510b1
Jiashun Wang <1043210554@qq.com>
2025-03-20 13:53:26 +0800

    add code

---
+++ scripts/download_ERA_daily.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   download_ERA_daily.py
+@Time    :   2025/03/20 13:51:27
+@Author  :   Jiashun Wang 
+@Version :   1.0
+@Contact :   wjs@ieee.org
+'''
+
+
+import cdsapi
+
+dataset = "derived-era5-single-levels-daily-statistics"
+request = {
+    "product_type": "reanalysis",
+    "variable": ["surface_net_solar_radiation"],
+    "year": "1993",
+    "month": ["01"],
+    "day": ["01"],
+    "daily_statistic": "daily_mean",
+    "time_zone": "utc+00:00",
+    "frequency": "1_hourly"
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
